9c0918d089803bdf30c6f83efae313c15e1237ff
Artur Oscar <Arturoscar3@gmail.com>
2021-10-11 20:21:27 -0300

    Adding class 2, flow control and procedural paradigm.

--- bytebank/src/main/kotlin/main.kt
+++ bytebank/src/main/kotlin/main.kt
@@ -1,16 +1,52 @@
-fun main(){
+fun main() {
     println("Bem vindo ao Bytebank, curso 01 do @Alura")
 
     /*Aula 01 - Variaveis*/
     // var -> variavel que seu valor pode ser modificado
     // val -> nao permite modificar seu valor. Comecar geralmente com val, se precisar muda para var.
-    val titular = "Artur Oscar"; // o kotlin converte implicitamente para o tipo.
-    val numeroConta: Int = 1000 // para forcar o tipo
-    var saldo = 0.0; // eh estaticamente tipado.
+    for (i in 1..5) { // o range com '..' deve ser crescente. Caso deseje que decremente, use downto.
+        val titular = "Artur Oscar $i"; // o kotlin converte implicitamente para o tipo.
+        val numeroConta: Int = 1000 + i // para forcar o tipo
+        var saldo = i + 10.0; // eh estaticamente tipado.
 
-    saldo = 100 + 2.0;
-    saldo += 200
-    println("titular: $titular")
-    println("Numero da conta: $numeroConta")
-    println("Saldo da conta: $saldo")
+        println("titular: $titular")
+        println("Numero da conta: $numeroConta")
+        println("Saldo da conta: $saldo")
+
+        testaCondicoes(saldo);
+        testaDownTo()
+        println()
+    }
+}
+
+fun testaDownTo() {
+    for(i in 5 downTo 1 step 2){ //para pular e parar, usa if com continue ou break.
+        println(i)
+    }
+    println()
+    var x = 0
+    while(x < 5){
+        println(x)
+        x++
+    }
+}
+
+fun testaCondicoes(saldo: Double) {
+
+    if (saldo > 0.0)
+        println("A conta eh positiva.")
+    else if (saldo == 0.0)
+        println("a conta esta zerada.")
+    else
+        println("A conta esta negativa.")
+
+    //equivalem-se.
+
+    when { // tipo um switch case do kotlin
+        saldo > 0.0 -> println("A conta eh positiva.")
+        saldo == 0.0 -> {
+            print("A conta esta zerada.")
+        }
+        else -> println("A conta esta negativa.")
+    }
 }
