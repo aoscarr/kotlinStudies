cf245470f6d86b8abc96cb507096128517c4f77a
Artur Oscar <Arturoscar3@gmail.com>
2021-10-18 19:41:17 -0300

    Finalizando curso de introdução à OO com Kotlin.

--- bytebank/src/main/kotlin/main.kt
+++ bytebank/src/main/kotlin/main.kt
@@ -2,28 +2,72 @@ fun main() {
     /*Aula 01 - Variaveis*/
     println("Bem vindo ao Bytebank, curso 01 do @Alura")
 
-    val contaArtur = Conta()
+    // da para por labels nos parametros, assim pode enviar na sua ordem.
+    val contaArtur = Conta(titular = "Artur", numero = 1000); // labels
     contaArtur.titular = "Artur"
-    contaArtur.numeroConta = 1001
-    contaArtur.saldo = 500.0
-
+    contaArtur.depositar(500.00)
     println("titular da conta: ${contaArtur.titular}")
     contaArtur.depositar(1200.0)
     println("saldo da conta: ${contaArtur.saldo}")
+
+    val contaDois = Conta(numero = 1001, titular = "Teste")
 }
 
-class Conta {
-    var titular = ""
-    var numeroConta = 0
-    var saldo = 0.0
+//sempre trabalha inicialmente com os valores em val. So muda se for necessario.
 
-    fun depositar(valor : Double){
-        if(valor < 0) {
+// pode declarar a var nos param
+class Conta( // deixa em uma linha ou deixa assim, caso tenha muitos param.
+    var titular: String, // para ser opcional, basta por os valores de inicializacao
+    val numero: Int = 0 // torna opcional
+
+) {
+    //var titular = titular
+    //var numeroConta = numero
+    var saldo = 1.0 // property, as var ja podem ter seus gets e sets proprios
+        private set(valor) { //nao precisa indicar o tipo
+            if (valor > 0)
+                field = valor; // field eh o valor interno da var
+        }
+        get
+    // chamar o construtor aqui, eh chamado de construtor secundario, eh possivel usar o primario,
+    // que eh na propria declaracao da classe.
+//    constructor(titular: String, numero: Int){ // construtor secundario
+//        this.titular = titular;
+//        this.numeroConta = numero;
+//    }
+
+    fun depositar(valor: Double) {
+        if (valor < 0) {
             println("Nao eh possivel depositar valor negativo.")
             return;
         }
         this.saldo += valor;
     }
+
+    fun sacar(valor: Double) {
+        if (this.saldo < valor) {
+            println("NEGADO: Saldo insuficiente.")
+            return;
+        }
+        this.saldo -= valor;
+    }
+
+    fun transferir(valor: Double, conta: Conta): Unit { // por padrao, retorna Unit
+        if (this.saldo < valor) {
+            println("NEGADO: Saldo insuficiente.")
+            return
+        }
+        conta.depositar(valor);
+        this.saldo -= valor;
+    }
+//    nao eh interessante get e set no kotlin.
+//    fun getSaldo(): Double{
+//        return this.saldo;
+//    }
+//    fun setSaldo(valor: Double){
+//        if(valor > 0)
+//            this.saldo = valor;
+//    }
 }
 
 fun testaLacos() {
@@ -44,12 +88,12 @@ fun testaLacos() {
         println()
     }
     // for decrescente.
-    for(i in 5 downTo 1 step 2){ //para pular e parar, usa if com continue ou break.
+    for (i in 5 downTo 1 step 2) { //para pular e parar, usa if com continue ou break.
         println(i)
     }
     println()
     var x = 0
-    while(x < 5){
+    while (x < 5) {
         println(x)
         x++
     }
